1983ac74a61c96dcc4ae1b3b30a3264eec98cdd3
Nurlan_Kubatbekov <kubatbekovnurlan13@gmail.com>
2022-01-08 15:05:53 +0600

    #4 Made 3 Stage

--- src/edu1/Main.java
+++ src/edu1/Main.java
@@ -16,17 +16,72 @@ public class Main {
         cats.add(new Cat("Cat 4", 13, 70, 70, 70));
         cats.add(new Cat("Cat 5", 9, 60, 60, 60));
 
+        Printer.printHeader();
+        cats.sort(Cat::sortByAverageLevel);
+        cats.forEach(c -> {
+            Printer.printCat(cats.indexOf(c) + 1, c);
+        });
 
-        Cat newCat = inputCat();
-        cats.add(newCat);
+        action(cats);
 
         Printer.printHeader();
         cats.sort(Cat::sortByAverageLevel);
         cats.forEach(c -> {
-            Printer.printCat(cats.indexOf(c), c);
+            Printer.printCat(cats.indexOf(c) + 1, c);
         });
 
 
+    }
+
+    public static void action(List<Cat> cats) {
+        System.out.println("You also could actions: ");
+        System.out.println("● Feed the cat      => 1");
+        System.out.println("● Play with the cat => 2");
+        System.out.println("● Treat the cat     => 3");
+        System.out.println("● Get a new pet     => A");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the comand: ");
+        String inputComand = sc.nextLine();
+
+        switch (inputComand) {
+            case "1":
+                makeActionToCat("Feed", cats);
+                break;
+            case "2":
+                makeActionToCat("Play", cats);
+                break;
+            case "3":
+                makeActionToCat("Treat", cats);
+                break;
+            case "a":
+            case "A":
+                Cat newCat1 = inputCat();
+                cats.add(newCat1);
+                break;
+
+
+        }
+    }
+
+    public static void makeActionToCat(String text, List<Cat> cats) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of cat: ");
+        String inputNumber = sc.nextLine();
+        int intInputNumber = 0;
+
+        try {
+            intInputNumber = tryInt(inputNumber);
+        } catch (ParseException e) {
+            System.out.println("Please enter correct number of Cat!");
+            makeActionToCat(text, cats);
+        }
+        if (0 >= intInputNumber || cats.size() < intInputNumber) {
+            System.out.println("There is no such cat!");
+        } else {
+            Cat findedCat = cats.get(intInputNumber - 1);
+            System.out.println("You " + text + ", cat : " + findedCat.getName() + ", age: " + findedCat.getAge());
+        }
     }
 
     public static Cat inputCat() {
